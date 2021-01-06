1c2e7aa2bff65440696dfd65e682980d52f3c430
Nathan Fiorito <nfiorito64@gmail.com>
2021-01-06 07:43:58 -0300

    No commit

--- src/pages/Homescreen/index.js
+++ src/pages/Homescreen/index.js
@@ -4,7 +4,7 @@ export default function Homescreen() {
   return (
       <div className="Title">
         <h1>Work in progress...</h1>
-        <h1 className="Barra">|</h1>
+        <h1 className="Barra"></h1>
       </div>
   );
 }
