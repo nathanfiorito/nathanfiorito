60446e2b84eb95007c1b500a09298c7ad9bd7a08
Nathan Fiorito <nfiorito64@gmail.com>
2021-01-06 03:55:09 -0300

    Starting development

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+import Homescreen from './pages/Homescreen';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Homescreen />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
---
+++ src/pages/Homescreen/index.js
@@ -0,0 +1,10 @@
+import './Homescreen.css'
+
+export default function Homescreen() {
+  return (
+      <div className="Title">
+        <h1>Work in progress...</h1>
+        <h1 className="Barra">|</h1>
+      </div>
+  );
+}
