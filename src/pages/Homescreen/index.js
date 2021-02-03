b335fffb11661c28df0c2f66be35733841a3ac3f
Nathan Fiorito <nfiorito64@gmail.com>
2021-02-03 16:00:05 -0300

    Identidade visual remodelada.

--- src/pages/Homescreen/index.js
+++ src/pages/Homescreen/index.js
@@ -8,25 +8,27 @@ import linkedinLogo from '../../assets/img/linkedinLogo.svg';
 export default function Homescreen() {
   return (
       <>
-        <div className='info'>
-          <div className='profile'>
-            <img src={Photo} className='profile-picture' alt='Profile'></img>
-            <h2 className='name'>Nathan Fiorito</h2>
-            <h3 className='developer'>FULL-STACK DEVELOPER</h3>
+        <div className='center-v flex-row'>
+          <div>
+            <img src={Photo} className='center profile-picture' alt='Profile'></img>
           </div>
-          <div className='social-medias'>
-            <div className='social-link-div'>
-              <img src={githubLogo} alt='Github Logo'></img>
-              <h3 className='social-link-text'>Visite o meu Github!</h3>
-            </div>
-            <div className='social-link-div'>
-              <img src={twitterLogo} alt='Twitter Logo'></img>
-              <h3 className='social-link-text'>Me siga no Twitter!</h3>
-            </div>
-            <div className='social-link-div'>
-              <img src={linkedinLogo} alt='Linkedin Logo'></img>
-              <h3 className='social-link-text'>Acesse o meu Linkedin!</h3>
-            </div>
+          <div className='flex-column center-h'>
+            <p className='center-h name'>Nathan Fiorito</p>
+            <p className='center-h developer'>FULL-STACK DEVELOPER</p>
+              <div className='center flex-row'>
+                <div className='social-link'>
+                  <img className='logo' src={githubLogo} alt='Github Logo'></img>
+                  <h3>Visite o meu Github!</h3>
+                </div>
+                <div className='social-link'>
+                  <img className='logo' src={twitterLogo} alt='Twitter Logo'></img>
+                  <h3>Me siga no Twitter!</h3>
+                </div>
+                <div className='social-link'>
+                  <img className='logo' src={linkedinLogo} alt='Linkedin Logo'></img>
+                  <h3>Acesse o meu Linkedin!</h3>
+                </div>
+              </div>
           </div>
         </div>
       </>
